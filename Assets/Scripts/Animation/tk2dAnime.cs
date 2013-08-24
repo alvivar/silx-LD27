f19e1741de566aeba13b06b76e836b1b37ffc227
alvivar <andresalvivar@gmail.com>
2013-08-24 17:21:37 -0600

    animation component placeholder.

---
+++ Assets/Scripts/Animation/tk2dAnime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Tk2d anime animates using tk2d and motion data.
+/// </summary>
+
+public class tk2dAnime : MonoBehaviour
+{
+
+	void Start()
+	{
+	
+	}
+
+	
+	void Update()
+	{
+	
+	}
+}
