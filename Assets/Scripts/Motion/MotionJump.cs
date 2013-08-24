bd2bc113d4b29d509d8efb58168d213f03a5dadf
alvivar <andresalvivar@gmail.com>
2013-08-24 14:34:40 -0600

    motion can jump now.

---
+++ Assets/Scripts/Motion/MotionJump.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Allows jump in motion when certain keycode is pressed.
+/// </summary>
+
+[RequireComponent( typeof ( Motion ) )]
+
+public class MotionJump : MonoBehaviour
+{
+	public KeyCode key = KeyCode.Space;
+	
+	public float jumpForce = 10f;
+		
+	private Motion motion;
+
+
+	void Start()
+	{
+		motion = GetComponent<Motion>();
+	}
+
+	
+	void Update()
+	{
+		if ( Input.GetKeyDown( key ) )
+		{
+			motion.backUpY = true;
+			
+			motion.movement.y = jumpForce;
+		}
+	}
+}
