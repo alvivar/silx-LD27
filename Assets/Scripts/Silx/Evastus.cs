179c3d7d92686f108452239ade6127c179b46881
alvivar <andresalvivar@gmail.com>
2013-08-26 00:45:41 -0600

    Evastus placeholder.

---
+++ Assets/Scripts/Silx/Evastus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Evastus : MonoBehaviour
+{
+	void Start()
+	{
+		Messenger.Broadcast( FXBlinkMediatorEvent.Set, 0.1f, 0.1f );
+	}
+
+
+	void Update()
+	{
+	
+	}
+}
