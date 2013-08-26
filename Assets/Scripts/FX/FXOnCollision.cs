4bede8d93f3f72cda488fefe51b9893f4e1284ab
alvivar <andresalvivar@gmail.com>
2013-08-25 19:06:42 -0600

    fx on rigidbody collisions.

---
+++ Assets/Scripts/FX/FXOnCollision.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// FX on collision with rigidbodies.
+/// </summary>
+
+public class FXOnCollision : MonoBehaviour
+{
+	public ParticleSystem particles;
+	
+	public float layer = 0;
+	
+	public bool destroyObject = false;
+
+	
+	void OnCollisionEnter( Collision collision )
+	{
+		if ( particles )
+		{
+			// create on the layer.
+			
+			Vector3 particlesPos = transform.position;
+			
+			particlesPos.z = layer;
+			
+			Instantiate( particles, particlesPos, Quaternion.identity );
+			
+			// destroy.
+			
+			if ( destroyObject )
+			{
+				Destroy( transform.gameObject );
+			}
+		}
+	}
+}
