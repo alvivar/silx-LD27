11327b335527eca5742de08b3b404956239ac6fe
alvivar <andresalvivar@gmail.com>
2013-08-25 19:06:08 -0600

    the camera mediator is now compatible with two objects.

--- Assets/Scripts/Camera/CameraMediator.cs
+++ Assets/Scripts/Camera/CameraMediator.cs
@@ -9,6 +9,8 @@ using System.Collections;
 public class CameraMediator : MonoBehaviour
 {
 	public Transform currentFocus;
+	
+	public Transform secondaryFocus;
 
 	public float cameraSpeed = 10f;
 
@@ -23,12 +25,29 @@ public class CameraMediator : MonoBehaviour
 
 	void Update()
 	{
+		// this is obvious.
+		
 		if ( !currentFocus )
 		{
 			return;
 		}
-
-		Vector3 newCameraPosition = new Vector3( currentFocus.position.x + offset.x, currentFocus.position.y + offset.y, transform.position.z );
+		
+		Vector3 newCameraPosition = Vector3.zero;
+		
+		// if the position is gonna be between two objects.
+		
+		if ( secondaryFocus )
+		{
+			float distanceBetween = Vector3.Distance( currentFocus.position, secondaryFocus.position );
+			
+			Vector3 positionBetween = ( currentFocus.position / 2 ) + ( secondaryFocus.position / 2 );
+			
+			newCameraPosition = new Vector3( positionBetween.x + offset.x, positionBetween.y + offset.y, transform.position.z );
+		}
+		else
+		{
+			newCameraPosition = new Vector3( currentFocus.position.x + offset.x, currentFocus.position.y + offset.y, transform.position.z );
+		}
         
 		transform.position = Vector3.Lerp( transform.position, newCameraPosition, Time.deltaTime * cameraSpeed );
 	}
